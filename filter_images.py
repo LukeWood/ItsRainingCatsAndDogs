ddc43b39f0b59ee83b03e26f6ee06713023dc84c
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2017-02-14 21:08:18 -0600

    filter script

---
+++ filter_images.py
@@ -0,0 +1,49 @@
+import struct
+import imghdr
+from shutil import copyfile
+import glob
+
+def get_image_size(fname):
+    '''Determine the image type of fhandle and return its size.
+    from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height
+
+MIN = 250
+MAX = 500
+
+for fname in glob.glob("data/*"):
+	w,h = get_image_size(fname)
+	if(w > MAX or w < MIN or h > MAX or h < MIN):
+		continue
+	copyfile(fname, fname.replace("data","data_filtered"))
+print("Percentage of images kept:",len(glob.glob("data_filtered/*"))/len(glob.glob("data/*")))
